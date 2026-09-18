a9518dd02da6e883db39449803936aff1d5c8a0f
Sebastian Vargas <s3basacero@gmail.com>
2026-09-18 11:13:10 -0500

    supabaseClient

--- src/shared/lib/supabaseClient.js
+++ src/shared/lib/supabaseClient.js
@@ -10,4 +10,6 @@ if (!supabaseUrl || !supabaseAnonKey) {
   );
 }
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = supabaseUrl && supabaseAnonKey
+  ? createClient(supabaseUrl, supabaseAnonKey)
+  : null;
